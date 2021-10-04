d176adb4e2537dedbf20534ad3e46853b374687a
OleksandrTymoshenko <nomakievip@gmail.com>
2021-10-04 22:35:18 +0300

    Elina_test

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,22 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileBtnClose = document.querySelector('[data-menu-close]');
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//   const mobileBtnClose = document.querySelector('[data-menu-close]');
 
-  menuBtnRef.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
+//   menuBtnRef.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
 
-  mobileBtnClose.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
+//   mobileBtnClose.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+(() => { const refs = { openMenuBtn: document.querySelector('.menu-open'), closeMenuBtn:
+document.querySelector('.menu-close'), menu: document.querySelector('.mob-menu'), };
+
+refs.openMenuBtn.addEventListener('click', toggleMenu);
+refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+    function toggleMenu() { refs.menu.classList.toggle('is-hidden'); }
 })();
